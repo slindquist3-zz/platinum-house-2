5c08cb05498305f8f0670b6d2b2a72cbf6f87c3f
Scott Lindquist <scott.lindquist@equinox.com>
2018-08-27 16:38:11 -0400

    Skeleton for sections

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
 (function(){
 //always load home section first
-  $(document).ready(function() {
+  // $(document).ready(function() {
+  //   $('#section-classes').hide();
+  //   $('#section-london-daries').hide();
+  //   $('#section-home').show();
+  // })
 
-    $('#section-classes').hide();
-    $('#section-london-daries').hide();
-    $('#section-home').show();
-
-  })
-
+  //navigation logic
   $('#nav-link-home').on('click', function() {
-
     $('#section-classes').hide();
     $('#section-london-daries').hide();
     $('#section-home').show();
-
   });
-
   $('#nav-link-classes').on('click', function() {
-
     $('#section-home').hide();
     $('#section-london-diaries').hide();
     $('#section-classes').show();
-
   });
-
   $('#nav-link-london-diaries').on('click', function() {
-
     $('#section-home').hide();
-    $('#section-london-diaries').hide();
+    $('#section-classes').hide();
     $('#section-london-diaries').show();
 
   });
+
+
+
+
 
 })();
