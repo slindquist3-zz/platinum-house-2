022d05576d6f4e3385ca1c61a6110c6c8a951e48
Scott Lindquist <scott.lindquist@equinox.com>
2018-08-27 15:00:15 -0400

    Add Javascript to switch between sections

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-console.log("Hi!");
+(function(){
+//always load home section first
+  $(document).ready(function() {
+
+    $('#section-classes').hide();
+    $('#section-london-daries').hide();
+    $('#section-home').show();
+
+  })
+
+  $('#nav-link-home').on('click', function() {
+
+    $('#section-classes').hide();
+    $('#section-london-daries').hide();
+    $('#section-home').show();
+
+  });
+
+  $('#nav-link-classes').on('click', function() {
+
+    $('#section-home').hide();
+    $('#section-london-diaries').hide();
+    $('#section-classes').show();
+
+  });
+
+  $('#nav-link-london-diaries').on('click', function() {
+
+    $('#section-home').hide();
+    $('#section-london-diaries').hide();
+    $('#section-london-diaries').show();
+
+  });
+
+})();
