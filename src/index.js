f120fefd887f9ed545b68b9a160e4307b865b442
Scott Lindquist <scott.lindquist@equinox.com>
2018-08-29 16:40:01 -0400

    Styles with Ji's feedback

--- src/index.js
+++ src/index.js
@@ -8,21 +8,29 @@
       $('#section-london-diaries').hide();
       $('#section-home').show();
 
+      $('#amex-logo').hide();
+
       // navigation logic
       $('#nav-link-home').on('click', function() {
         $('#section-classes').hide();
         $('#section-london-diaries').hide();
         $('#section-home').show();
+        $('#amex-logo').hide();
+
       });
       $('#nav-link-classes').on('click', function() {
         $('#section-home').hide();
         $('#section-london-diaries').hide();
         $('#section-classes').show();
+        $('#amex-logo').show();
+
       });
       $('#nav-link-london-diaries').on('click', function() {
         $('#section-home').hide();
         $('#section-classes').hide();
         $('#section-london-diaries').show();
+        $('#amex-logo').show();
+
 
       });
 
@@ -34,38 +42,39 @@
         $('#section-home').show();
         $('#section-classes').show();
         $('#section-london-diaries').show();
-      } else {
-
-        var links = $('.nav-link-text');
-
-        //check nav elements
-        //then control sections
-
-        $.each(links, function(i, el) {
-
-          var section;
-
-          if ($(el).hasClass('link-active')) {
-            console.log("this one!");
-
-            section = "#" + $(el).attr('data-name');
-
-          }
-
-        })
-
-        $.each($('section'), function(index, el) {
-
-          if ($(el).attr('id') === section) {
-            $(el).show();
-          } else {
-            $(el).hide();
-          }
-
-        })
-
-
       }
+      // else {
+      //
+      //   var links = $('.nav-link-text');
+      //
+      //   //check nav elements
+      //   //then control sections
+      //
+      //   $.each(links, function(i, el) {
+      //
+      //     var section;
+      //
+      //     if ($(el).hasClass('link-active')) {
+      //       console.log("this one!");
+      //
+      //       section = "#" + $(el).attr('data-name');
+      //
+      //     }
+      //
+      //   })
+      //
+      //   $.each($('section'), function(index, el) {
+      //
+      //     if ($(el).attr('id') === section) {
+      //       $(el).show();
+      //     } else {
+      //       $(el).hide();
+      //     }
+      //
+      //   })
+      //
+      //
+      // }
 
     });
 
